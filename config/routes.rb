243b6ff53d2d82ab022cf78eec77c1619b6a3f10
Giovanni Barros <barrosgiovanni@icloud.com>
2023-01-18 14:06:39 -0300

    changed order of attributes in the index.json

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: "pages#home"
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :books, only: [:index]
     end
   end
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
