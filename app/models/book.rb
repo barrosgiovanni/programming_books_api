a120de2cfda1013ebd751c80471d60c6d9f3a1c6
Giovanni Barros <barrosgiovanni@icloud.com>
2023-01-18 10:11:50 -0300

    added validations to models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,9 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_many :reviews, dependent: :destroy
+
+  validates :title, :author, :year, :category, :isbn, :book_url, :description, presence: true
+  validates :description, length: { in: 30..300 }
+  validates :book_url, length: { minimum: 20 }
+  validates :isbn, length: { in: 10..13 }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :book
   belongs_to :user
+
+  validates :score, :content, presence: true
+  validates :score, numericality: { in: 1..5 }
+  validates :content, length: { in: 10..200 }
 end
