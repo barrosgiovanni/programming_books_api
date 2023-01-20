0d98ebb2eb8195d9aa5125d35c895a9b0391d048
Giovanni Barros <barrosgiovanni@icloud.com>
2023-01-20 16:50:51 -0300

    added update method and other required functions in books controller

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,11 +1,35 @@
 class Api::V1::BooksController < Api::V1::BaseController
+  before_action :set_book, only: %i[show update]
+
   def index
     @books = policy_scope(Book)
     # @books = Book.all
   end
 
   def show
+  end
+
+  def update
+    if @book.update(book_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
+  private
+
+  def set_book
     @book = Book.find(params[:id])
     authorize @book
+  end
+
+  def book_params
+    params.require(:book).permit(:id, :title, :author, :publisher, :year, :category, :description, :isbn, :pages, :image_url, :book_url, :rating)
+  end
+
+  def render_error
+    render json: { errors: @book.errors.full_messages },
+    status: :unprocessable_entity
   end
 end
