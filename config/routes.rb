591c33d09fddaaf6f51843ed8a65861139eef21c
Giovanni Barros <barrosgiovanni@icloud.com>
2023-01-20 16:30:14 -0300

    added update route to routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :books, only: %i[index show]
+      resources :books, only: %i[index show update]
     end
   end
 end
